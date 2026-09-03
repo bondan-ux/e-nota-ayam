03e1b772d00fc5ba9f6b002d05094bfd44c4971e
bondan-ux <bondanherlambang2000@gmail.com>
2026-09-04 03:27:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ def generate_image_nota(tgl, bakul, group, items, total_bayar, logo_path):
         except TypeError:
             return ImageFont.load_default()
 
-    font_title = load_font(bold_fonts, 32)
+    font_title = load_font(bold_fonts, 25)
     font_total = load_font(bold_fonts, 36)
     font_bold = load_font(bold_fonts, 16)
     font_regular = load_font(reg_fonts, 13)
